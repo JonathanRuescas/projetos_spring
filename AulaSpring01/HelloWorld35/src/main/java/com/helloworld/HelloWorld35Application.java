5f22c7afa42789d70b5c656a56a61379e3a0c291
Jonathan Ruescas de Matos <82004857+JonathanRuescas@users.noreply.github.com>
2021-10-27 18:10:22 -0300

    add trefa

---
+++ AulaSpring01/HelloWorld35/src/main/java/com/helloworld/HelloWorld35Application.java
@@ -0,0 +1,13 @@
+package com.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorld35Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorld35Application.class, args);
+	}
+
+}
